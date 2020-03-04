34dd9ba5e8ced5de40dbef2f3a1a867d4cea15cb
trdbau <trdbau@gmail.com>
2020-03-04 21:17:18 +0700

    init commit

---
+++ src/views/example/components/Dropdown/Comment.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-dropdown :show-timeout="100" trigger="click">
+    <el-button plain>{{ !comment_disabled?'OPENED COMMENTS':'CLOSED COMMENTS' }}
+      <i class="el-icon-caret-bottom el-icon--right"/>
+    </el-button>
+    <el-dropdown-menu slot="dropdown" class="no-padding">
+      <el-dropdown-item>
+        <el-radio-group v-model="comment_disabled" style="padding: 10px;">
+          <el-radio :label="true">CLOSE COMMENTS</el-radio>
+          <el-radio :label="false">OPEN COMMENTS</el-radio>
+        </el-radio-group>
+      </el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    comment_disabled: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
